19faf7bb84508d9297252eaf46ab29258893e406
Dmitry <lmahaon@yandex.ru>
2024-11-29 15:42:06 +0300

    Add custom 404, 403 errors

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-m&$lzdzkutvrbr5vt=jpm)7#g7cken_tk%($ty+w902n7wb#=e'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost']
@@ -106,4 +106,7 @@ EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
 # редирект после авторизации
 LOGIN_REDIRECT_URL = 'birthday:list_cbv'
 # адрес для логина
-LOGIN_URL = 'login' 
+LOGIN_URL = 'login'
+
+# кастомная обработка ошибки csrf
+CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
